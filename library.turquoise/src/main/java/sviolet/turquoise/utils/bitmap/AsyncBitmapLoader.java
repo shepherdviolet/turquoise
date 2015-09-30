7e5fdd9e815270395fe055249b4504cdfffdf235
S.Violet <ryan.n@163.com>
2015-09-30 12:58:17 +0800

    注释修改

--- library.turquoise/src/main/java/sviolet/turquoise/utils/bitmap/AsyncBitmapLoader.java
+++ library.turquoise/src/main/java/sviolet/turquoise/utils/bitmap/AsyncBitmapLoader.java
@@ -293,16 +293,22 @@ public class AsyncBitmapLoader {
 
         /**
          * 实现根据url参数从网络下载图片数据, 依照需求尺寸reqWidth和reqHeight解析为合适大小的Bitmap,
-         * 并调用结果容器resultHolder.set(Bitmap)方法将Bitmap返回, 若加载失败则set(null)<br/>
+         * 并调用结果容器resultHolder.set(Bitmap)方法将Bitmap返回, 若加载失败则set(null).<br/>
          * <br/>
-         * 注意:<br/>
-         * 1.网络请求注意做超时处理,否则任务可能会一直等待<br/>
-         * 2.数据解析为Bitmap时,请根据需求尺寸reqWidth和reqHeight解析, 以节省内存<br/>
+         * * * 注意::<br/>
          * <br/>
-         * 线程会阻塞等待,直到resultHolder.set(Bitmap)方法执行.若任务被cancel,阻塞也会被中断,且即使后续
+         * 1.网络请求注意做超时处理,否则任务可能会一直等待.<br/>
+         * <br/>
+         * 2.数据解析为Bitmap时,请根据需求尺寸reqWidth和reqHeight解析,以节省内存.<br/>
+         * @see BitmapUtils <br/>
+         * * * 需求尺寸定义::<br/>
+         * * * 需求尺寸(reqWidth/reqHeight)参数用于节省内存消耗,请根据界面展示所需尺寸设置(像素px).<br/>
+         * * * 图片解码时会根据需求尺寸整数倍缩小,且长宽保持原图比例,解码后的Bitmap尺寸通常不等于需求尺寸.设置为0不缩小图片.<Br/>
+         * <br/>
+         * 4.线程会阻塞等待,直到resultHolder.set(Bitmap)方法执行.若任务被cancel,阻塞也会被中断,且即使后续
          * 网络请求返回了Bitmap,也会被Bitmap.recycle().<br/>
          * <br/>
-         * 无论同步还是异步的情况,均使用resultHolder.set(Bitmap)返回结果<br/>
+         * 5.无论同步还是异步的情况,均使用resultHolder.set(Bitmap)返回结果.<br/>
          * 同步网络请求:<br/>
          *
          *      //网络加载代码
@@ -353,6 +359,9 @@ public class AsyncBitmapLoader {
      * AsyncBitmapLoader中每个位图资源都由url唯一标识, url在AsyncBitmapLoader内部
      * 将由getCacheKey()方法计算为一个cacheKey, 内存缓存/磁盘缓存/队列key都将使用
      * 这个cacheKey标识唯一的资源<br/>
+     * <Br/>
+     * 需求尺寸(reqWidth/reqHeight)参数用于节省内存消耗,请根据界面展示所需尺寸设置(像素px).图片解码时会
+     * 根据需求尺寸整数倍缩小,且长宽保持原图比例,解码后的Bitmap尺寸通常不等于需求尺寸.设置为0不缩小图片.<Br/>
      *
      * @param url 图片URL地址
      * @param reqWidth 需求宽度 px
