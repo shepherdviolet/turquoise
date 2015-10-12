578f23d85d1478171565f995bd09aad1369c0252
S.Violet <ryan.n@163.com>
2015-10-12 13:52:18 +0800

    注释修改

--- library.turquoise/src/main/java/sviolet/turquoise/utils/bitmap/loader/BitmapLoaderHolder.java
+++ library.turquoise/src/main/java/sviolet/turquoise/utils/bitmap/loader/BitmapLoaderHolder.java
@@ -8,11 +8,22 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * BitmapLoader结果容器<br/>
  * 用途:<Br/>
- * 1.同步/异步返回结果:setResultSucceed/setResultFailed/setResultCanceled</><br/>
- * 2.判断当前任务是否被取消:canceled<br/>
- * 3.可设置任务取消监听器.配合异步网络框架使用,回调方法中终止网络请求:setOnCancelListenner<br/>
+ * 1.返回结果:setResultSucceed/setResultFailed/setResultCanceled<br/>
+ *      无论同步处理,还是异步处理,均通过这些方法返回结果,BitmapLoader加载任务中的getResult方法会阻塞,
+ *      直到setResultSucceed/setResultFailed/setResultCanceled方法被调用.<br/>
+ *      1)setResultSucceed(Bitmap),加载成功,返回Bitmap<br/>
+ *      2)setResultFailed(Throwable),加载失败,返回异常<br/>
+ *      3)setResultCanceled(),加载取消<br/>
+ *      若加载任务已被取消(isCancelling() = true),但仍使用setResultSucceed返回结果,则Bitmap会被存入
+ *      磁盘缓存,BitmapLoader返回任务取消.<br/>
+ * 2.判断任务是否取消中:isCancelling<br/>
+ *      加载任务是否被取消.通常用于同步加载场合,从InputStream循环读取数据时,判断isCancelling(),若为
+ *      true,则终止读取,并setResultCanceled()返回结果.<br/>
+ * 3.设置任务取消监听器:setOnCancelListener<br/>
+ *      加载任务被取消时,会回调该监听器.通常用于异步加载场合,例如使用异步网络框架,在该监听器回调方法中
+ *      调用框架方法,终止网络请求.<br/>
  * <Br/>
- * BitmapLoader加载任务中的getResult方法会阻塞, 一直到setResult或setThrowable方法被调用<br/>
+ *
  */
 public class BitmapLoaderHolder {
 
@@ -28,13 +39,13 @@ public class BitmapLoaderHolder {
 
     private Bitmap bitmap;//结果图
     private Throwable throwable;//异常
-    private Runnable onCancelListenner;//取消监听器
+    private Runnable onCancelListener;//取消监听器
 
     private final ReentrantLock lock = new ReentrantLock();
     private final Condition condition = lock.newCondition();
 
     /**
-     * 返回结果:加载成功<br/>
+     * [重要]返回结果:加载成功<br/>
      */
     public void setResultSucceed(Bitmap bitmap){
         lock.lock();
@@ -56,7 +67,7 @@ public class BitmapLoaderHolder {
     }
 
     /**
-     * 返回结果:加载失败<br/>
+     * [重要]返回结果:加载失败<br/>
      */
     public void setResultFailed(Throwable t){
         lock.lock();
@@ -74,7 +85,7 @@ public class BitmapLoaderHolder {
     }
 
     /**
-     * 返回结果:加载取消
+     * [重要]返回结果:加载取消<br/>
      */
     public void setResultCanceled(){
         lock.lock();
@@ -94,7 +105,7 @@ public class BitmapLoaderHolder {
     /**
      * 判断任务当前是否正在被取消<br/>
      * 该状态由内部方法cancel()触发, 并非处理最终结果(result), 并非由setResultCanceled决定,
-     * 仅表示该任务是否需要进行取消处理, 不表示已被取消成功<br/>
+     * 仅表示该任务需要进行取消处理, 不表示已被取消成功<br/>
      */
     public boolean isCancelling(){
         lock.lock();
@@ -109,9 +120,13 @@ public class BitmapLoaderHolder {
      * 设置取消监听器<br/>
      * 当任务被取消时, 会回调该监听器, 注意监听器线程可能为UI线程<br/>
      */
-    public void setOnCancelListenner(Runnable onCancelListenner){
-        this.onCancelListenner = onCancelListenner;
+    public void setOnCancelListener(Runnable onCancelListener){
+        this.onCancelListener = onCancelListener;
     }
+
+    /*************************************************************
+     * inner
+     */
 
     /**
      * 阻塞等待结果
@@ -150,15 +165,15 @@ public class BitmapLoaderHolder {
 
     /**
      * 取消任务<br/>
-     * 1.置为被取消状态<br/>
+     * 1.置为正在取消状态<br/>
      * 2.回调取消监听器<br/>
      */
     void cancel(){
         lock.lock();
         try{
             isCancelling = true;
-            if (onCancelListenner != null)
-                onCancelListenner.run();
+            if (onCancelListener != null)
+                onCancelListener.run();
         } finally {
             lock.unlock();
         }
@@ -188,7 +203,7 @@ public class BitmapLoaderHolder {
         try{
             this.bitmap = null;
             this.throwable = null;
-            this.onCancelListenner = null;
+            this.onCancelListener = null;
         } finally {
             lock.unlock();
         }
