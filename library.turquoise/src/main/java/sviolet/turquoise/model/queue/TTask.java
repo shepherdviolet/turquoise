bf29b1815c754c8aceadefb062728174252458ef
S.Violet <ryan.n@163.com>
2015-10-19 19:12:00 +0800

    对TQueue的持有改为弱引用

--- library.turquoise/src/main/java/sviolet/turquoise/model/queue/TTask.java
+++ library.turquoise/src/main/java/sviolet/turquoise/model/queue/TTask.java
@@ -1,5 +1,6 @@
 package sviolet.turquoise.model.queue;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
@@ -8,15 +9,15 @@ import java.util.TimerTask;
 /**
  * TQueue队列用任务<br>
  * <br>
- * 
+ *
  * @author S.Violet
  *
  */
 
 public abstract class TTask {
-	
+
 	//static//////////////////////////////////////////
-	
+
 	public static final int STATE_WAITTING = 0;//等待被启动
 	public static final int STATE_PRE_EXECUTE = 1;//执行前
 	public static final int STATE_EXECUTING = 2;//正在执行
@@ -24,27 +25,28 @@ public abstract class TTask {
 	public static final int STATE_COMPLETE = 4;//执行完毕
     public static final int STATE_CANCELING = 5;//取消中
     public static final int STATE_CANCELED = 6;//已取消
-	
+
 	//setting//////////////////////////////////////////
+
+    private WeakReference<TQueue> mQueue;//队列
 
 	private Object params;//参数
 	private Object result;//结果
-	private TQueue queue;//任务队列
 	private long timeOutSet = -1;//任务超时设置
 	private String key;//标签
 	private boolean cancelable = true;//是否允许被取消(中止)
 
 	private List<TTask> follower;//跟随的任务
-	
+
 	//var//////////////////////////////////////////
 
 	private int state = STATE_WAITTING;//执行状态
 	private Timer timeOutTimer;//超时计时器
-	
+
 	/*********************************************
 	 * GETSET
 	 */
-	
+
 	/**
 	 * 获取当前任务状态<br>
      * STATE_WAITTING = 0;//等待被启动
@@ -54,19 +56,21 @@ public abstract class TTask {
      * STATE_COMPLETE = 4;//执行完毕
      * STATE_CANCELING = 5;//取消中
      * STATE_CANCELED = 6;//已取消
-	 * 
+	 *
 	 * @return
 	 */
 	public int getState() {
 		return state;
 	}
-	
+
 	/**
 	 * 获取绑定的队列
 	 * @return
 	 */
 	public TQueue getQueue() {
-		return queue;
+		if (mQueue != null)
+            return mQueue.get();
+        return null;
 	}
 
 	/**
@@ -74,9 +78,9 @@ public abstract class TTask {
 	 * @param queue
 	 */
 	public void setQueue(TQueue queue) {
-		this.queue = queue;
+		this.mQueue = new WeakReference<TQueue>(queue);
 	}
-	
+
 	/**
 	 * 获取参数
 	 * @return
@@ -93,7 +97,7 @@ public abstract class TTask {
 		this.params = params;
 		return this;
 	}
-	
+
 	/**
 	 * 设置任务超时时间, 超时后会调用cancel方法
 	 * @param timeout 默认不超时-1
@@ -101,7 +105,7 @@ public abstract class TTask {
 	public void setTimeOut(long timeout){
 		this.timeOutSet = timeout;
 	}
-	
+
 	/**
 	 * 设置任务是否允许被手动中止(超时中止无视该设置)
 	 * @param cancelable 默认true
@@ -173,15 +177,15 @@ public abstract class TTask {
             }
             state = STATE_PRE_EXECUTE;
         }
-		if (queue != null) {
-			queue.ttask_postStart(this);
+		if (getQueue() != null) {
+            getQueue().ttask_postStart(this);
 		}else {
             throw new RuntimeException("[TTask]can not start a TTask without TQueue");
 		}
 		//启动成功
 		return true;
 	}
-	
+
 	/**
 	 * 中止任务(手动)<br>
 	 * 若设置cancelable = false, 则此方法无效<br>
@@ -190,11 +194,11 @@ public abstract class TTask {
 		if(cancelable)
 			onCancel();
 	}
-	
+
 	/**
 	 * 当终止任务时调用(超时或手动中止)[复写]<br>
 	 * <br>
-	 * 复写该方法实现任务的中断, 例如网络请求abort等操作, 
+	 * 复写该方法实现任务的中断, 例如网络请求abort等操作,
 	 * 父类方法已调用setStopState()设置任务状态为结束,
 	 * 并置空线程对象, 通知队列调度
 	 */
@@ -209,8 +213,8 @@ public abstract class TTask {
             }
         }
 		cancelTimeOutTimer();
-		if(queue != null){
-			queue.notifyDispatchTask();
+		if(getQueue() != null){
+            getQueue().notifyDispatchTask();
 		}
 	}
 
@@ -224,21 +228,21 @@ public abstract class TTask {
 	/******************************************************************
 	 * abstract
 	 */
-	
+
 	/**
 	 * 任务前准备(主线程), 通常为UI操作
 	 * @param params
 	 * @return
 	 */
 	public abstract void onPreExecute(Object params);
-	
+
 	/**
 	 * 任务(新线程)
 	 * @param params
 	 * @return
 	 */
 	public abstract Object doInBackground(Object params);
-	
+
 	/**
 	 * 任务后回调(主线程), 通常为UI操作<br/>
      * 执行中的任务被cancel, 任务结束后仍会调用该方法<br/>
@@ -247,11 +251,11 @@ public abstract class TTask {
      * @param isCancel 任务"取消中"/"已取消"状态为true
 	 */
 	public abstract void onPostExecute(Object result, boolean isCancel);
-	
+
 	/**************************************************************************
 	 * Private
 	 */
-	
+
 	/**
 	 * 任务执行过程(主线程)
 	 */
@@ -260,10 +264,10 @@ public abstract class TTask {
             return;
         onPreExecute(params);
 		resetTimeOutTimer();
-		if (queue == null){
+		if (getQueue() == null){
 			throw new RuntimeException("[TTask]can not start a TTask without TQueue");
 		}
-		queue.ttask_execute(new Runnable() {
+        getQueue().ttask_execute(new Runnable() {
             @Override
             public void run() {
                 synchronized (TTask.this) {
@@ -278,8 +282,8 @@ public abstract class TTask {
                         return;
                     state = STATE_POST_EXECUTE;
                 }
-                if (queue != null) {
-                    queue.ttask_postComplete(TTask.this);
+                if (getQueue() != null) {
+                    getQueue().ttask_postComplete(TTask.this);
                 }
             }
         });
@@ -297,8 +301,8 @@ public abstract class TTask {
                 state = STATE_CANCELED;
             }
         }
-		if(queue != null){
-			queue.notifyDispatchTask();
+		if(getQueue() != null){
+            getQueue().notifyDispatchTask();
 		}
 
         //执行跟随者
@@ -336,8 +340,8 @@ public abstract class TTask {
     private boolean checkCancelState() {
         //检查取消状态
         if (state >= STATE_CANCELING){
-            if(queue != null){
-                queue.ttask_postComplete(TTask.this);
+            if(getQueue() != null){
+                getQueue().ttask_postComplete(TTask.this);
             }
             return true;
         }
@@ -360,7 +364,7 @@ public abstract class TTask {
 			}
 		}, timeOutSet);
 	}
-	
+
 	/**
 	 * 取消超时计时器
 	 */
@@ -375,10 +379,11 @@ public abstract class TTask {
 	 * 销毁任务(主线程)
 	 */
 	protected void onDestroy(){
-		queue = null;
 		timeOutTimer = null;//超时计时器
 		params = null;
 		result = null;
+        if (follower != null)
+            follower.clear();
 	}
 
 }
