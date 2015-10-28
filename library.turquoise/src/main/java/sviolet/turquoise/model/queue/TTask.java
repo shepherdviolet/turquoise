d1a61d815c7796d7aa0161af5ab13af6bee6427c
S.Violet <ryan.n@163.com>
2015-10-28 09:34:32 +0800

    版权说明

--- library.turquoise/src/main/java/sviolet/turquoise/model/queue/TTask.java
+++ library.turquoise/src/main/java/sviolet/turquoise/model/queue/TTask.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/turquoise
+ * Email: shepherdviolet@163.com
  */
 
 package sviolet.turquoise.model.queue;
