17b9fcef3a8078909e9e4001d67d900fffada83c
S.Violet <ryan.n@163.com>
2015-11-27 16:02:33 +0800

    增加方法

--- turquoise/src/main/java/sviolet/turquoise/enhanced/TActivity.java
+++ turquoise/src/main/java/sviolet/turquoise/enhanced/TActivity.java
@@ -108,11 +108,37 @@ public class TActivity extends Activity implements TActivityProvider.RequestPerm
      * 检查权限->显示说明->请求权限->回调{@link TActivityProvider.RequestPermissionTask}<br/>
      * 目的任务在{@link TActivityProvider.RequestPermissionTask}中实现, 需要判断权限是否被授予<br/>
      *
+     * @param permission 所需权限 android.Manifest.permission....
+     * @param task 需要权限的任务
+     */
+    public void executePermissionTask(String permission, TActivityProvider.RequestPermissionTask task){
+        provider.executePermissionTask(this, new String[]{permission}, null, null, task);
+    }
+
+    /**
+     * 执行一个需要权限的任务, 兼容低版本<br/>
+     * 检查权限->显示说明->请求权限->回调{@link TActivityProvider.RequestPermissionTask}<br/>
+     * 目的任务在{@link TActivityProvider.RequestPermissionTask}中实现, 需要判断权限是否被授予<br/>
+     *
      * @param permissions 所需权限 android.Manifest.permission....
      * @param task 需要权限的任务
      */
     public void executePermissionTask(String[] permissions, TActivityProvider.RequestPermissionTask task){
         provider.executePermissionTask(this, permissions, null, null, task);
+    }
+
+    /**
+     * 执行一个需要权限的任务, 兼容低版本<br/>
+     * 检查权限->显示说明->请求权限->回调{@link TActivityProvider.RequestPermissionTask}<br/>
+     * 目的任务在{@link TActivityProvider.RequestPermissionTask}中实现, 需要判断权限是否被授予<br/>
+     *
+     * @param permission 所需权限 android.Manifest.permission....
+     * @param rationaleTitle 权限说明标题(标题和内容都送空, 则不提示)
+     * @param rationaleContent 权限说明内容(标题和内容都送空, 则不提示)
+     * @param task 需要权限的任务
+     */
+    public void executePermissionTask(String permission, String rationaleTitle, String rationaleContent, TActivityProvider.RequestPermissionTask task){
+        provider.executePermissionTask(this, new String[]{permission}, rationaleTitle, rationaleContent, task);
     }
 
     /**
