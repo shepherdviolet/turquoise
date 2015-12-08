6013f0c81321b9c4b1e278cc702de52c4056fc25
S.Violet <ryan.n@163.com>
2015-12-08 13:56:21 +0800

    性能优化: 22s/亿次 -> 7s/亿次

--- turquoise/src/main/java/sviolet/turquoise/utils/reflect/ObjectTracker.java
+++ turquoise/src/main/java/sviolet/turquoise/utils/reflect/ObjectTracker.java
@@ -43,11 +43,20 @@ import java.util.Map;
  *
  * 获取成员变量:<br/>
  * <pre>{@code
- *      Bean bean = new Bean();
+ *      //JavaBean
+ *      Bean bean1 = new Bean();
+ *      Bean bean2 = new Bean();
+ *      Bean bean3 = new Bean();
  *      ...
- *      Object name1 = ObjectTracker.get(bean, "elements.0.name");
- *      Object name2 = ObjectTracker.get(bean, "elements.1.name");
- *      Object name3 = ObjectTracker.get(bean, {"elements", "2", "name"});
+ *      //访问路径(键组)
+ *      ObjectTracker.KeyGroup keyGroup = new ObjectTracker.KeyGroup("elements.0.name");
+ *      //ObjectTracker.KeyGroup keyGroup = new ObjectTracker.KeyGroup(new String[]{"elements", "2", "name"});
+ *      ...
+ *      //获取JavaBean的成员
+ *      //keyGroup内部会持有JavaBean的Field, 使用同一个keyGroup反复获取能够提升性能
+ *      Object name1 = ObjectTracker.get(bean1, keyGroup);
+ *      Object name2 = ObjectTracker.get(bean2, keyGroup);
+ *      Object name3 = ObjectTracker.get(bean3, keyGroup);
  * }</pre>
  *
  * Created by S.Violet on 2015/11/3.
@@ -55,51 +64,15 @@ import java.util.Map;
 public class ObjectTracker {
 
     /**
-     * 获取源对象的成员变量, 异常返回null
-     *
-     * @param source 源对象
-     * @param keyGroup 访问路径(键组) 例:branches.2.name
-     */
-    public static Object fetch(Object source, String keyGroup){
-        try{
-            return get(source, keyGroup);
-        }catch (Exception ignored){
-        }
-        return null;
-    }
-
-    /**
-     * 获取源对象的成员变量, 异常返回null
-     *
-     * @param source 源对象
-     * @param keyGroup 访问路径(键组) 例:{"branches","2","name"}
-     */
-    public static Object fetch(Object source, String[] keyGroup){
-        try{
-            return get(source, keyGroup);
-        }catch (Exception ignored){
-        }
-        return null;
-    }
-
-    /**
      * 获取源对象的成员变量
      *
      * @param source 源对象
-     * @param keyGroup 访问路径(键组) 例:branches.2.name
+     * @param keyGroup 访问路径(键组)
      * @exception Exception 获取失败抛出异常
      */
-    public static Object get(Object source, String keyGroup) throws Exception {
-        if (keyGroup == null || "".equals(keyGroup)){
-            throw new Exception("[ObjectTracker]keyGroup must not be null");
-        }
-        //根据分隔符切开键组
-        String[] keys = keyGroup.split("\\.");
-        if (keys.length <= 0){
-            throw new Exception("[ObjectTracker]illegal keyGroup:<" + keyGroup + ">, no key elements");
-        }
+    public static Object get(Object source, KeyGroup keyGroup) throws Exception {
         try {
-            return get(source, keys);
+            return innerGet(source, keyGroup);
         }catch(Exception e){
             throw new Exception("[ObjectTracker]illegal keyGroup:<" + keyGroup + ">", e);
         }
@@ -109,18 +82,21 @@ public class ObjectTracker {
      * 获取源对象的成员变量
      *
      * @param source 源对象
-     * @param keyGroup 访问路径(键组) 例:{"branches","2","name"}
+     * @param keyGroup 访问路径(键组)
      * @exception Exception 获取失败抛出异常
      */
-    public static Object get(Object source, String[] keyGroup) throws Exception {
-        if (keyGroup == null || keyGroup.length <= 0){
-            throw new Exception("[ObjectTracker]keyGroup must not be null or empty");
+    private static Object innerGet(Object source, KeyGroup keyGroup) throws Exception {
+        if (keyGroup == null){
+            throw new Exception("[ObjectTracker]illegal keyGroup, keyGroup must not be null");
+        }
+        if (keyGroup.keys == null){
+            throw new Exception("[ObjectTracker]illegal keyGroup:<" + keyGroup.illegalKeyGroup + ">, keyGroup must not be null or empty");
         }
 
         //当前访问对象
         Object current = source;
         //遍历键组
-        for(String key : keyGroup){
+        for(int i = 0 ; i < keyGroup.keys.length ; i++){
             /*
                 对象为空
              */
@@ -132,7 +108,7 @@ public class ObjectTracker {
                 对象为Map
              */
             else if(current instanceof Map){
-                current = ((Map) current).get(key);
+                current = ((Map) current).get(keyGroup.keys[i]);
             }
             /*
                 对象为List
@@ -141,15 +117,14 @@ public class ObjectTracker {
                 //编号
                 int index;
                 try{
-                    index = Integer.parseInt(key);
+                    index = Integer.parseInt(keyGroup.keys[i]);
                 }catch(Exception e){
-                    throw new Exception("[ObjectTracker]illegal key:<" + key + ">, key must be an integer while in List element", e);
+                    throw new Exception("[ObjectTracker]illegal key:<" + keyGroup.keys[i] + ">, key must be an integer while in List element", e);
                 }
                 //获取对象
                 try {
                     current = ((List) current).get(index);
                 }catch (Exception e){
-                    //out of bounds 视为元素不存在
                     return null;
                 }
             }
@@ -160,15 +135,14 @@ public class ObjectTracker {
                 //编号
                 int index;
                 try{
-                    index = Integer.parseInt(key);
+                    index = Integer.parseInt(keyGroup.keys[i]);
                 }catch(Exception e){
-                    throw new Exception("[ObjectTracker]illegal key:<" + key + ">, key must be an integer while in Array element", e);
+                    throw new Exception("[ObjectTracker]illegal key:<" + keyGroup.keys[i] + ">, key must be an integer while in Array element", e);
                 }
                 //获取对象
                 try {
                     current = Array.get(current, index);
                 }catch (Exception e){
-                    //out of bounds 视为元素不存在
                     return null;
                 }
             }
@@ -178,12 +152,23 @@ public class ObjectTracker {
             else{
                 //当前访问的类
                 Class currentClass = current.getClass();
-                //目标Field
-                Field field = null;
+                //目标Field, 从键组里获取Field缓存
+                Field field = keyGroup.getField(i);
+                //若键组里缓存存在Field, 则尝试用该Field获取
+                if (field != null){
+                    //获取对象
+                    try {
+                        current = field.get(current);
+                    }catch (Exception e){
+                        //获取失败, 尝试重新查找Field
+                        field = null;
+                    }
+                }
+                //若键组缓存不存在Field, 则查找Field
                 while(field == null) {
                     //当前访问类中获取Field
                     try {
-                        field = currentClass.getDeclaredField(key);
+                        field = currentClass.getDeclaredField(keyGroup.keys[i]);
                     } catch (Exception ignored) {
                     }
                     //Field不存在
@@ -194,23 +179,87 @@ public class ObjectTracker {
                         if (currentClass == null || currentClass == Object.class){
                             break;
                         }
+                    }else{
+                        keyGroup.setField(i, field);
+                        //获取对象
+                        try {
+                            current = field.get(current);
+                        }catch (Exception e){
+                            throw new Exception("[ObjectTracker]can not find target element in map (reflect way), key:<" + keyGroup.keys[i] + ">", e);
+                        }
                     }
                 }
                 //Field不存在
                 if (field == null){
-                    throw new Exception("[ObjectTracker]illegal key:<" + key + ">, can not find target field in map (reflect way)");
-                }
-                //获取对象
-                try {
-                    field.setAccessible(true);
-                    current = field.get(current);
-                }catch (Exception e){
-                    throw new Exception("[ObjectTracker]can not find target element in map (reflect way), key:<" + key + ">", e);
+                    throw new Exception("[ObjectTracker]illegal key:<" + keyGroup.keys[i] + ">, can not find target field in map (reflect way)");
                 }
             }
         }
 
         return current;
+    }
+
+    /**
+     * 访问路径(键组)<p/>
+     *
+     * 内部持有JavaBean的Field, 重复访问可加速<p/>
+     */
+    public static class KeyGroup{
+
+        private String[] keys;//分割的路径
+        private Field[] fields;//分割路径对应的Field
+        private String illegalKeyGroup;//非法的路径
+
+        /**
+         * @param keyGroup 访问路径(键组), 例:elements.0.name
+         * @throws Exception 路径非法抛出异常
+         */
+        public KeyGroup(String keyGroup) {
+            if (keyGroup == null || "".equals(keyGroup)){
+                illegalKeyGroup = "null";
+                return;
+            }
+            //根据分隔符切开键组
+            String[] keys = keyGroup.split("\\.");
+            if (keys.length <= 0){
+                illegalKeyGroup = keyGroup;
+                return;
+            }
+            this.keys = keys;
+            this.fields = new Field[this.keys.length];
+        }
+
+        /**
+         *
+         * @param keyGroup 访问路径(键组), 例:{"elements","0","name"}
+         * @throws Exception 路径非法抛出异常
+         */
+        public KeyGroup(String[] keyGroup) {
+            if (keyGroup == null || keyGroup.length <= 0){
+                illegalKeyGroup = "null";
+                return;
+            }
+            this.keys = keyGroup;
+            this.fields = new Field[this.keys.length];
+        }
+
+        private void setField(int index, Field field){
+            if (index < 0 || index >= fields.length)
+                return;
+            field.setAccessible(true);
+            synchronized (this) {
+                fields[index] = field;
+            }
+        }
+
+        private Field getField(int index){
+            if (index < 0 || index >= fields.length)
+                return null;
+            synchronized (this){
+                return fields[index];
+            }
+        }
+
     }
 
 }
