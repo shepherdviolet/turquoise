2487cd1890d96bebd5e7b9c88bb148d1dd4b9bda
S.Violet <ryan.n@163.com>
2016-01-06 11:04:21 +0800

    增加路径工具方法

--- turquoise/src/main/java/sviolet/turquoise/utils/sys/DirectoryUtils.java
+++ turquoise/src/main/java/sviolet/turquoise/utils/sys/DirectoryUtils.java
@@ -18,7 +18,6 @@ public class DirectoryUtils {
     /**
      * 应用对应外部文件路径
      * @param context context
-     * @return
      */
     public static File getExternalFilesDir(Context context){
         return context.getExternalFilesDir(null);
@@ -28,19 +27,33 @@ public class DirectoryUtils {
      * 应用对应外部文件路径
      * @param context context
      * @param type Environment.DIRECTORY_...
-     * @return
      */
     public static File getExternalFilesDir(Context context, String type){
         return context.getExternalFilesDir(type);
     }
 
     /**
-     * 应用对应内部文件路径
-     * @param context
-     * @return
+     * 应用对应数据路径/data/data/<application package>/
+     * @param context context
      */
-    public static File getFileDir(Context context){
-        return context.getFilesDir();
+    public static File getDataDir(Context context){
+        return new File(context.getApplicationInfo().dataDir);
+    }
+
+    /**
+     * 应用源码路径 /data/app/<application package>/base.apk
+     * @param context context
+     */
+    public static File getSourceDir(Context context){
+        return new File(context.getApplicationInfo().sourceDir);
+    }
+
+    /**
+     * 应用native库路径 /data/app/<application package>/lib/arm
+     * @param context context
+     */
+    public static File getNativeLibraryDir(Context context){
+        return new File(context.getApplicationInfo().nativeLibraryDir);
     }
 
     /************************
@@ -51,7 +64,7 @@ public class DirectoryUtils {
      * 应用对应的缓存路径, 动态选择优先外部储存<br/>
      * 外部储存存在时, 返回/sdcard/Android/data/<application package>/cache/subDir
      * 外部储存不存在, 返回/data/data/<application package>/cache/subDir
-     * @param context
+     * @param context context
      * @param subDir 子目录
      */
     public static File getCacheDir(Context context, String subDir) {
@@ -63,7 +76,7 @@ public class DirectoryUtils {
      * 应用对应的缓存路径, 动态选择优先外部储存<br/>
      * 外部储存存在时, 返回/sdcard/Android/data/<application package>/cache
      * 外部储存不存在, 返回/data/data/<application package>/cache
-     * @param context
+     * @param context context
      */
     public static File getCacheDir(Context context) {
         File pathFile = getExternalCacheDir(context);
@@ -75,7 +88,7 @@ public class DirectoryUtils {
 
     /**
      * 应用对应的外部缓存路径, 若不存在返回null
-     * @param context
+     * @param context context
      * @return /sdcard/Android/data/<application package>/cache
      */
     public static File getExternalCacheDir(Context context){
@@ -88,7 +101,7 @@ public class DirectoryUtils {
 
     /**
      * 应用对应的内部缓存路径
-     * @param context
+     * @param context context
      * @return /data/data/<application package>/cache
      */
     public static File getInnerCacheDir(Context context){
