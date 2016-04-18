f5bd279258d13b5b9e704613185e6f0b47932260
shepherdviolet <ryan.n@163.com>
2016-04-18 13:02:44 +0800

    测试代码

--- demoa.imageloader/src/main/java/sviolet/demoaimageloader/demos/BasicActivity.java
+++ demoa.imageloader/src/main/java/sviolet/demoaimageloader/demos/BasicActivity.java
@@ -3,11 +3,17 @@ package sviolet.demoaimageloader.demos;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import java.util.concurrent.locks.LockSupport;
+
 import sviolet.demoaimageloader.R;
 import sviolet.demoaimageloader.common.DemoDescription;
 import sviolet.turquoise.enhance.app.TActivity;
 import sviolet.turquoise.enhance.app.annotation.inject.ResourceId;
 import sviolet.turquoise.enhance.app.annotation.setting.ActivitySettings;
+import sviolet.turquoise.utilx.tlogger.TLogger;
+import sviolet.turquoise.x.imageloader.TILoader;
+import sviolet.turquoise.x.imageloader.drawable.ContainerDrawable;
+import sviolet.turquoise.x.imageloader.drawable.def.DefaultLoadingDrawableFactory;
 
 /**
  * Basic Usage of TurquoiseImageLoader
@@ -35,7 +41,23 @@ public class BasicActivity extends TActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        TILoader.node(this).load("url", imageView);
+        TILoader.node(this).load("https://avatars0.githubusercontent.com/u/12589661?v=3&s=460", imageView);
+
+//        final DefaultLoadingDrawableFactory factory = new DefaultLoadingDrawableFactory();
+//        factory.setBackgroundResId(R.mipmap.ic_launcher);
+//        factory.setBackgroundColor(0xFF702020);
+//        factory.setPointColor(0xFFFF0000);
+//        factory.setAnimationEnabled(false);
+
+//        imageView.setImageDrawable(new ContainerDrawable(factory.create(getApplicationContext(), this, null, TLogger.get(this))));
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                LockSupport.parkNanos(10 * 1000000000L);
+//                factory.onDestroy();
+//            }
+//        }).start();
 
     }
 }
