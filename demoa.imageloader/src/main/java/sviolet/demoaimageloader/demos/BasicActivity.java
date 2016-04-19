b915b0ca5bdac9d7f99d56ad4c9e3501b755d198
shepherdviolet <ryan.n@163.com>
2016-04-19 18:03:56 +0800

    demo修改

--- demoa.imageloader/src/main/java/sviolet/demoaimageloader/demos/BasicActivity.java
+++ demoa.imageloader/src/main/java/sviolet/demoaimageloader/demos/BasicActivity.java
@@ -3,21 +3,20 @@ package sviolet.demoaimageloader.demos;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-import java.util.concurrent.locks.LockSupport;
+import java.io.IOException;
 
 import sviolet.demoaimageloader.R;
 import sviolet.demoaimageloader.common.DemoDescription;
 import sviolet.turquoise.enhance.app.TActivity;
 import sviolet.turquoise.enhance.app.annotation.inject.ResourceId;
 import sviolet.turquoise.enhance.app.annotation.setting.ActivitySettings;
-import sviolet.turquoise.utilx.tlogger.TLogger;
 import sviolet.turquoise.x.imageloader.TILoader;
-import sviolet.turquoise.x.imageloader.drawable.ContainerDrawable;
-import sviolet.turquoise.x.imageloader.drawable.def.DefaultLoadingDrawableFactory;
+import sviolet.turquoise.x.imageloader.TILoaderUtils;
+import sviolet.turquoise.x.imageloader.entity.ServerSettings;
 
 /**
  * Basic Usage of TurquoiseImageLoader
- *
+ * <p/>
  * Created by S.Violet on 2016/3/8.
  */
 
@@ -37,27 +36,23 @@ public class BasicActivity extends TActivity {
     @ResourceId(R.id.basic_main_imageview)
     private ImageView imageView;
 
+//    private Params params = new Params.Builder()
+//            .setBitmapConfig(Bitmap.Config.ARGB_8888)
+//            .build();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        TILoader.setting(new ServerSettings.Builder().setMemoryCachePercent(this, 0.001f).build());
+
+        try {
+            TILoaderUtils.wipeDiskCache(this, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         TILoader.node(this).load("https://avatars0.githubusercontent.com/u/12589661?v=3&s=460", imageView);
-
-//        final DefaultLoadingDrawableFactory factory = new DefaultLoadingDrawableFactory();
-//        factory.setBackgroundResId(R.mipmap.ic_launcher);
-//        factory.setBackgroundColor(0xFF702020);
-//        factory.setPointColor(0xFFFF0000);
-//        factory.setAnimationEnabled(false);
-
-//        imageView.setImageDrawable(new ContainerDrawable(factory.create(getApplicationContext(), this, null, TLogger.get(this))));
-//
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                LockSupport.parkNanos(10 * 1000000000L);
-//                factory.onDestroy();
-//            }
-//        }).start();
 
     }
 }
