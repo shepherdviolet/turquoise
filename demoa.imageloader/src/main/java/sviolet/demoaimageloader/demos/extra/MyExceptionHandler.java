1b1af0cda81ef20cc835e0fdc40304600883a082
shepherdviolet <ryan.n@163.com>
2016-04-27 16:39:54 +0800

    demo修改

---
+++ demoa.imageloader/src/main/java/sviolet/demoaimageloader/demos/extra/MyExceptionHandler.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015-2016 S.Violet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/turquoise
+ * Email: shepherdviolet@163.com
+ */
+
+package sviolet.demoaimageloader.demos.extra;
+
+import android.content.Context;
+import android.util.Log;
+
+import sviolet.turquoise.utilx.tlogger.TLogger;
+import sviolet.turquoise.x.imageloader.handler.ExceptionHandler;
+import sviolet.turquoise.x.imageloader.node.Task;
+
+/**
+ * 自定义实现异常处理: 改为用Log打印
+ *
+ * Created by S.Violet on 2016/4/27.
+ */
+public class MyExceptionHandler implements ExceptionHandler {
+
+    @Override
+    public void onDiskCacheOpenException(Context applicationContext, Context context, Throwable throwable, TLogger logger) {
+        Log.e("MyExceptionHandler", "onDiskCacheOpenException", throwable);
+    }
+
+    @Override
+    public void onDiskCacheReadException(Context applicationContext, Context context, Task.Info taskInfo, Throwable throwable, TLogger logger) {
+        Log.e("MyExceptionHandler", "onDiskCacheReadException", throwable);
+    }
+
+    @Override
+    public void onDiskCacheWriteException(Context applicationContext, Context context, Task.Info taskInfo, Throwable throwable, TLogger logger) {
+        Log.e("MyExceptionHandler", "onDiskCacheWriteException", throwable);
+    }
+
+    @Override
+    public void onDiskCacheCommonException(Context applicationContext, Context context, Throwable throwable, TLogger logger) {
+        Log.e("MyExceptionHandler", "onDiskCacheCommonException", throwable);
+    }
+
+    @Override
+    public void onNetworkLoadException(Context applicationContext, Context context, Task.Info taskInfo, Throwable throwable, TLogger logger) {
+        Log.e("MyExceptionHandler", "onNetworkLoadException", throwable);
+    }
+
+    @Override
+    public void onDecodeException(Context applicationContext, Context context, Task.Info taskInfo, Throwable throwable, TLogger logger) {
+        Log.e("MyExceptionHandler", "onDecodeException", throwable);
+    }
+}
