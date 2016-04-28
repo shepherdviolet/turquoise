cd35fb966dd30409bbfd5e187e3d44a990085dca
shepherdviolet <ryan.n@163.com>
2016-04-28 14:11:13 +0800

    注释修改

--- turquoise/src/main/java/sviolet/turquoise/ui/drawable/RoundedCornerBitmapDrawable.java
+++ turquoise/src/main/java/sviolet/turquoise/ui/drawable/RoundedCornerBitmapDrawable.java
@@ -64,23 +64,38 @@ public class RoundedCornerBitmapDrawable extends Drawable {
     }
 
     /**
-     * 绘制步骤1:外部控件通过该方法获得尺寸, 该尺寸供外部控件参考, 但不一定适用
+     * <p>绘制步骤1:控件(或外部Drawable)通过该方法获得尺寸, 该尺寸供外部控件参考, 但不一定适用.</p>
+     *
+     * <p>返回-1时, 表示建议拉伸填充外部.</p>
+     *
+     * <p>ImageView场合示例: ImageView尺寸为300*300, drawable尺寸(getIntrinsicWidth/Height)为200*200,
+     * IImageView.scaleType=center时, 会裁剪canvas, 使得canvas.getWidth()/getHeight()为控件自身尺寸300*300,
+     * 而canvas.getClipBounds()为Drawable的尺寸300*300(通过getIntrinsicWidth()/getIntrinsicHeight()获得).
+     * IImageView.scaleType=centerCrop时, 会裁剪canvas, 使得canvas.getWidth()/getHeight()为控件自身尺寸300*300,
+     * 而canvas.getClipBounds()为Drawable的尺寸200*200(通过getIntrinsicWidth()/getIntrinsicHeight()获得).
+     * IImageView.scaleType=fitXY时, 会裁剪canvas, 使得canvas.getWidth()/getHeight()为控件自身尺寸300*300,
+     * 而canvas.getClipBounds()为Drawable的尺寸300*300(通过getIntrinsicWidth()/getIntrinsicHeight()获得).</p>
      */
     @Override
     public int getIntrinsicWidth() {
         return width;
     }
 
-    /**
-     * 绘制步骤1:外部控件通过该方法获得尺寸, 该尺寸供外部控件参考, 但不一定适用.
-     */
     @Override
     public int getIntrinsicHeight() {
         return height;
     }
 
     /**
-     * 绘制步骤2: 外部控件决定该Drawable的绘制范围, 通过该方法设置范围.
+     * <p>绘制步骤2: 控件(或外部Drawable)决定该Drawable的绘制范围后, 通过该方法设置范围.</p>
+     *
+     * <p>控件通常会自动调用该方法. 在Drawable嵌套的场合(或其他特殊场合), 需要手动调用该方法指定绘制范围,
+     * 通常将外部Drawable的绘制范围设置给内部Drawable即可, 例如:drawable.setBounds(getBounds());</p>
+     *
+     * <p>ImageView场合示例: ImageView尺寸为300*300, drawable尺寸(getIntrinsicWidth/Height)为200*200,
+     * IImageView.scaleType=center时, 会通过该方法设置DrawableBounds为200*200.
+     * IImageView.scaleType=centerCrop时, 会通过该方法设置DrawableBounds为200*200.
+     * IImageView.scaleType=fitXY时, 会通过该方法设置DrawableBounds为300*300.</p>
      */
     @Override
     public void setBounds(Rect bounds) {
@@ -88,10 +103,22 @@ public class RoundedCornerBitmapDrawable extends Drawable {
     }
 
     /**
-     * 绘制步骤3: 绘制图形, 通常根据getBounds()决定绘制范围, drawable.getBounds()获得的范围可能比canvas.getClipBounds()的
-     * 值小, 因为canvas.getClipBounds()是整个画布的绘制范围, 而drawable.getBounds()是Drawable被允许绘制的范围. 另外,
-     * canvas.getWidth()和getHeight()的值为画布的真实尺寸, 通常为绘制范围的数倍(比例缩放), 可以根据canvas.getWidth()/canvasRect.width()
-     * 的比值计算缩放比例.
+     * <p>绘制步骤3: 绘制图形, 通常根据getBounds()决定绘制范围.</p>
+     *
+     * <p>drawable.getBounds()是控件(或外部Drawable)建议的绘制范围, 通常采用该尺寸绘制图形.</p>
+     *
+     * <p>canvas.getWidth()和getHeight()是画布的实际尺寸(像素), 忽略缩放的情况.</p>
+     *
+     * <p>canvas.getClipBounds()是画布的裁剪范围, 可能大于或小于画布实际尺寸, 通常, 画布的缩放会导致裁剪范围变化.
+     * 若绘制时需要填充整个控件, 可以使用该尺寸绘制图形. 若需要获得外层缩放比例, 可以通过如下方式计算:</p>
+     *
+     * <pre>{@code
+     *      Rect canvasRect = new Rect();
+     *      canvas.getClipBounds(canvasRect);
+     *      //计算横向缩放比例
+     *      float canvasWidthScale = (float)canvasRect.width() / (float)canvas.getWidth();
+     * }</pre>
+     *
      */
     @Override
     public void draw(Canvas canvas) {
