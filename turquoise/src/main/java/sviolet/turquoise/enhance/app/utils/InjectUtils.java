e0b1e4f40aca277a41e7360f54b3d17ae0fe3dff
shepherdviolet <ryan.n@163.com>
2016-08-05 09:17:11 +0800

    移除监听器注入

--- turquoise/src/main/java/sviolet/turquoise/enhance/app/utils/InjectUtils.java
+++ turquoise/src/main/java/sviolet/turquoise/enhance/app/utils/InjectUtils.java
@@ -22,12 +22,8 @@ package sviolet.turquoise.enhance.app.utils;
 import android.app.Activity;
 import android.view.View;
 
-import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
-import sviolet.turquoise.enhance.app.annotation.inject.OnClickMethod;
 import sviolet.turquoise.enhance.app.annotation.inject.ResourceId;
 
 /**
@@ -35,7 +31,6 @@ import sviolet.turquoise.enhance.app.annotation.inject.ResourceId;
  * <br/>
  * 1.根据"类"的@ResourceId注释注入对应的Activity布局文件<br/>
  * 2.根据"成员变量"的@ResourceId注释注入布局中对应的View对象<br/>
- * 3.根据"成员方法"的@OnClickMethod注释绑定对应View的点击监听器, 方法仅有一个View参数时传入OnClickListener的View参数, 不建议使用<Br/>
  *
  * Created by S.Violet on 2015/8/3.
  */
@@ -49,7 +44,6 @@ public class InjectUtils {
     public static void inject(Activity activity){
         InjectUtils.injectClassAnnotation(activity);// 注入类的注释
         InjectUtils.injectFieldAnnotation(activity);// 注入成员变量的注释
-//        InjectUtils.injectMethodAnnotation(activity);// 注入成员方法的注释, 不建议使用
     }
 
     /**
@@ -102,70 +96,70 @@ public class InjectUtils {
         }
     }
 
-    /**
-     * 根据成员方法的注释注入<br/>
-     * 3.根据"成员方法"的@OnClickMethod注释绑定对应View的点击监听器<Br/>
-     *
-     * @deprecated i don't like this way
-     */
-    @Deprecated
-    public static void injectMethodAnnotation(Activity activity){
-        injectMethodAnnotation(activity, activity.getClass());
-    }
-
-    /**
-     * 根据成员方法的注释注入<br/>
-     */
-    private static void injectMethodAnnotation(Activity activity, Class<?> clazz){
-        Method[] methods = clazz.getDeclaredMethods();
-        int resourceId;
-        View view;
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(OnClickMethod.class)){
-                resourceId = method.getAnnotation(OnClickMethod.class).value();
-                view = activity.findViewById(resourceId);
-                if (view == null)
-                    throw new InjectException("[InjectUtils]inject [" + method.getName() + "]'s OnClickMethod failed, can't find resource");
-                final Method finalMethod = method;
-                final WeakReference<Activity> weakActivity = new WeakReference<>(activity);
-                view.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Activity activity = weakActivity.get();
-                        if (activity != null) {
-                            invokeMethod(finalMethod, activity, v);
-                        }
-                    }
-                });
-            }
-        }
-        Class superClazz = clazz.getSuperclass();
-        if (!Activity.class.equals(superClazz)) {
-            injectMethodAnnotation(activity, superClazz);
-        }
-    }
-
-    /**
-     * 执行方法
-     *
-     * @param finalMethod 方法
-     * @param obj 执行的实例
-     * @param v 参数(View)
-     */
-    private static void invokeMethod(Method finalMethod, Object obj, View v) {
-        try {
-            final Class<?>[] parameterTypes = finalMethod.getParameterTypes();
-            finalMethod.setAccessible(true);
-            if (parameterTypes.length == 1 && View.class.equals(parameterTypes[0])) {
-                finalMethod.invoke(obj, v);
-            }else{
-                finalMethod.invoke(obj, new Object[parameterTypes.length]);
-            }
-        }catch(IllegalAccessException e){
-            throw new InjectException("[InjectUtils]invoke method [" + finalMethod.getName() + "] failed");
-        }catch(InvocationTargetException e){
-            throw new InjectException("[InjectUtils]invoke method [" + finalMethod.getName() + "] failed");
-        }
-    }
+//    /**
+//     * 根据成员方法的注释注入<br/>
+//     * 3.根据"成员方法"的@OnClickMethod注释绑定对应View的点击监听器<Br/>
+//     *
+//     * @deprecated i don't like this way
+//     */
+//    @Deprecated
+//    public static void injectMethodAnnotation(Activity activity){
+//        injectMethodAnnotation(activity, activity.getClass());
+//    }
+//
+//    /**
+//     * 根据成员方法的注释注入<br/>
+//     */
+//    private static void injectMethodAnnotation(Activity activity, Class<?> clazz){
+//        Method[] methods = clazz.getDeclaredMethods();
+//        int resourceId;
+//        View view;
+//        for (Method method : methods) {
+//            if (method.isAnnotationPresent(OnClickMethod.class)){
+//                resourceId = method.getAnnotation(OnClickMethod.class).value();
+//                view = activity.findViewById(resourceId);
+//                if (view == null)
+//                    throw new InjectException("[InjectUtils]inject [" + method.getName() + "]'s OnClickMethod failed, can't find resource");
+//                final Method finalMethod = method;
+//                final WeakReference<Activity> weakActivity = new WeakReference<>(activity);
+//                view.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Activity activity = weakActivity.get();
+//                        if (activity != null) {
+//                            invokeMethod(finalMethod, activity, v);
+//                        }
+//                    }
+//                });
+//            }
+//        }
+//        Class superClazz = clazz.getSuperclass();
+//        if (!Activity.class.equals(superClazz)) {
+//            injectMethodAnnotation(activity, superClazz);
+//        }
+//    }
+//
+//    /**
+//     * 执行方法
+//     *
+//     * @param finalMethod 方法
+//     * @param obj 执行的实例
+//     * @param v 参数(View)
+//     */
+//    private static void invokeMethod(Method finalMethod, Object obj, View v) {
+//        try {
+//            final Class<?>[] parameterTypes = finalMethod.getParameterTypes();
+//            finalMethod.setAccessible(true);
+//            if (parameterTypes.length == 1 && View.class.equals(parameterTypes[0])) {
+//                finalMethod.invoke(obj, v);
+//            }else{
+//                finalMethod.invoke(obj, new Object[parameterTypes.length]);
+//            }
+//        }catch(IllegalAccessException e){
+//            throw new InjectException("[InjectUtils]invoke method [" + finalMethod.getName() + "] failed");
+//        }catch(InvocationTargetException e){
+//            throw new InjectException("[InjectUtils]invoke method [" + finalMethod.getName() + "] failed");
+//        }
+//    }
 
 }
