74b3ef29f79f8eaa5acdc68815c9d30e2daed5ea
shepherdviolet <ryan.n@163.com>
2016-09-24 03:52:08 +0800

    developing

--- turquoise/src/main/java/sviolet/turquoise/uix/viewgesturectrl/ViewGestureControllerImpl.java
+++ turquoise/src/main/java/sviolet/turquoise/uix/viewgesturectrl/ViewGestureControllerImpl.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import sviolet.turquoise.enhance.common.WeakHandler;
-import sviolet.turquoise.util.common.MathUtils;
 
 /**
  * <p>View触摸控制器实现类</p>
@@ -37,8 +36,8 @@ import sviolet.turquoise.util.common.MathUtils;
  */
 public class ViewGestureControllerImpl implements ViewGestureController {
 
-    private static final int VERTICAL_ANGLE_TOLERANCE = 20;//判定为垂直角的允许容差
-
+//    private static final int VERTICAL_ANGLE_TOLERANCE = 30;//判定为垂直角的允许容差
+//
     //settings/////////////////////////////////////////////////
 
     private long longClickElapse = 1000;//长按时间
@@ -194,149 +193,155 @@ public class ViewGestureControllerImpl implements ViewGestureController {
                 if (touchPointGroup.getPointNum() <= 0) {
                     //无触点, 返回release状态
                     stateToRelease();
-                } else if (touchPointGroup.getMaxPointNum() >= 2) {
-                    //多触点, 判断是平移还是缩放还是旋转
-                    //TODO
+                } else if (touchPointGroup.getPointNum() >= 2) {
+                    stateToMultiTouch();
                 } else {
                     //单触点, 判断平移距离
                     ViewGestureTouchPoint point = touchPointGroup.getPoint(0);
                     if (point != null && point.isEffectiveMoved) {
-                        stateToMove();
+                        stateToSingleTouch();
                     }
                 }
                 break;
-            case MOVE:
-
+            case SINGLE_TOUCH:
+                if (touchPointGroup.getPointNum() <= 0) {
+                    //无触点, 返回release状态
+                    stateToRelease();
+                } else if (touchPointGroup.getPointNum() >= 2) {
+                    stateToMultiTouch();
+                }
                 break;
-            case MOVE_AND_ZOOM:
-
-                break;
-            case ROTATE:
-
+            case MULTI_TOUCH:
+                if (touchPointGroup.getPointNum() <= 0) {
+                    //无触点, 返回release状态
+                    stateToRelease();
+                } else if (touchPointGroup.getPointNum() == 1) {
+                    stateToSingleTouch();
+                }
                 break;
             default:
                 break;
         }
     }
 
-    private boolean isRotateGesture(ViewGestureTouchPoint pointSrc, ViewGestureTouchPoint pointDst){
-        //两个触点间距过小, 不判定为旋转手势
-        float pointDistance = calculatePointDistance(pointSrc, pointDst);
-        if (pointDistance < rotateGesturePointDistanceThreshold){
-            return false;
-        }
-        //两个触点之中有一个没有有效位移, 则不判定为旋转手势
-        if (!isPointMoving(pointSrc) || !isPointMoving(pointDst)){
-            return false;
-        }
-        float twoPointVectorAngle = calculatePointMotionVectorAngle(pointDst.currX - pointSrc.currX, pointDst.currY - pointDst.currY);
-        float srcVectorAngle = calculatePointMotionVectorAngle(pointSrc.stepX, pointSrc.stepY);
-        float dstVectorAngle = calculatePointMotionVectorAngle(pointDst.stepX, pointDst.stepY);
+//    private boolean isRotateGesture(ViewGestureTouchPoint pointSrc, ViewGestureTouchPoint pointDst){
+//        //两个触点间距过小, 不判定为旋转手势
+//        float pointDistance = calculatePointDistance(pointSrc, pointDst);
+//        if (pointDistance < rotateGesturePointDistanceThreshold){
+//            return false;
+//        }
+//        //两个触点之中有一个没有有效位移, 则不判定为旋转手势
+//        if (!isPointMoving(pointSrc) || !isPointMoving(pointDst)){
+//            return false;
+//        }
+//        float twoPointVectorAngle = calculatePointMotionVectorAngle(pointDst.currX - pointSrc.currX, pointDst.currY - pointDst.currY);
+//        float srcVectorAngle = calculatePointMotionVectorAngle(pointSrc.stepX, pointSrc.stepY);
+//        float dstVectorAngle = calculatePointMotionVectorAngle(pointDst.stepX, pointDst.stepY);
+//
+//        //触点位移矢量角度与触点连线垂直且反向时, 认定为旋转
+//        int isSrcMovingVertical = calculateMovingVertical(twoPointVectorAngle, srcVectorAngle);
+//        int isDstMovingVertical = calculateMovingVertical(twoPointVectorAngle, dstVectorAngle);
+//        if (isSrcMovingVertical > 0 && isDstMovingVertical < 0){
+//            return true;
+//        }
+//        if (isSrcMovingVertical < 0 && isDstMovingVertical > 0){
+//            return true;
+//        }
+//
+//        return false;
+//    }
 
-        //触点位移矢量角度与触点连线垂直且反向时, 认定为旋转
-        int isSrcMovingVertical = calculateMovingVertical(twoPointVectorAngle, srcVectorAngle);
-        int isDstMovingVertical = calculateMovingVertical(twoPointVectorAngle, dstVectorAngle);
-        if (isSrcMovingVertical > 0 && isDstMovingVertical < 0){
-            return true;
-        }
-        if (isSrcMovingVertical < 0 && isDstMovingVertical > 0){
-            return true;
-        }
+//    private float calculatePointDistance(ViewGestureTouchPoint pointSrc, ViewGestureTouchPoint pointDst) {
+//        double _x = Math.abs(pointDst.currX - pointSrc.currX);
+//        double _y = Math.abs(pointDst.currY - pointSrc.currY);
+//        return (float) Math.sqrt(_x * _x + _y * _y);
+//    }
 
-        return false;
-    }
-
-    private float calculatePointDistance(ViewGestureTouchPoint pointSrc, ViewGestureTouchPoint pointDst) {
-        double _x = Math.abs(pointDst.currX - pointSrc.currX);
-        double _y = Math.abs(pointDst.currY - pointSrc.currY);
-        return (float) Math.sqrt(_x * _x + _y * _y);
-    }
-
-    private boolean isPointMoving(ViewGestureTouchPoint point){
-        if (point.stepX > mTouchSlop || point.stepY > mTouchSlop){
-            return true;
-        }
-        return false;
-    }
+//    private boolean isPointMoving(ViewGestureTouchPoint point){
+//        if (Math.abs(point.stepX) > mTouchSlop || Math.abs(point.stepY) > mTouchSlop){
+//            return true;
+//        }
+//        return false;
+//    }
 
     /**
      * 计算点运动的矢量角度, 12点钟位置为0, 顺时针增加
      */
-    private float calculatePointMotionVectorAngle(float offsetX, float offsetY){
-        if (offsetX == 0 && offsetY == 0){
-            //点没有任何位移, 此处理论上不可能发生, 因为isRotateGesture方法中先判断了点是否有效位移
-            return 0;
-        }
-        if (offsetX == 0){
-            if (offsetY > 0){
-                return 180;
-            }else{
-                return 0;
-            }
-        }
-        if (offsetY == 0){
-            if (offsetX > 0){
-                return 90;
-            } else{
-                return 270;
-            }
-        }
-        if (offsetX > 0){
-            if (offsetY > 0){
-                return (float) (180 - MathUtils.atanAngle(Math.abs(offsetX / offsetY)));
-            } else {
-                return (float) (MathUtils.atanAngle(Math.abs(offsetX / offsetY)));
-            }
-        } else {
-            if (offsetY > 0){
-                return (float) (180 + MathUtils.atanAngle(Math.abs(offsetX / offsetY)));
-            } else {
-                return (float) (360 - MathUtils.atanAngle(Math.abs(offsetX / offsetY)));
-            }
-        }
-    }
+//    private float calculatePointMotionVectorAngle(float offsetX, float offsetY){
+//        if (offsetX == 0 && offsetY == 0){
+//            //点没有任何位移, 此处理论上不可能发生, 因为isRotateGesture方法中先判断了点是否有效位移
+//            return 0;
+//        }
+//        if (offsetX == 0){
+//            if (offsetY > 0){
+//                return 180;
+//            }else{
+//                return 0;
+//            }
+//        }
+//        if (offsetY == 0){
+//            if (offsetX > 0){
+//                return 90;
+//            } else{
+//                return 270;
+//            }
+//        }
+//        if (offsetX > 0){
+//            if (offsetY > 0){
+//                return (float) (180 - MathUtils.atanAngle(Math.abs(offsetX / offsetY)));
+//            } else {
+//                return (float) (MathUtils.atanAngle(Math.abs(offsetX / offsetY)));
+//            }
+//        } else {
+//            if (offsetY > 0){
+//                return (float) (180 + MathUtils.atanAngle(Math.abs(offsetX / offsetY)));
+//            } else {
+//                return (float) (360 - MathUtils.atanAngle(Math.abs(offsetX / offsetY)));
+//            }
+//        }
+//    }
 
     /**
      * 判断pointVectorAngle是否在lineVectorAngle垂直方向上, 允许一定的容差, 若在垂直方向上, 且角度大于
      * lineVectorAngle则返回1, 若在垂直方向上, 且角度小于lineVectorAngle, 则返回-1, 若不在垂直方向上则返回0
      */
-    private int calculateMovingVertical(float lineVectorAngle, float pointVectorAngle){
-        //逆时针方向的垂直角范围
-        float leftVerticalAngleMin = lineVectorAngle - 90 - VERTICAL_ANGLE_TOLERANCE;
-        float leftVerticalAngleMax = lineVectorAngle - 90 + VERTICAL_ANGLE_TOLERANCE;
-        if (leftVerticalAngleMin < 0 && leftVerticalAngleMax >= 0){
-            if (pointVectorAngle >= 0 && pointVectorAngle <= leftVerticalAngleMax){
-                return -1;//逆时针方向垂直
-            }
-            if (pointVectorAngle >= MathUtils.standardizeAngle(leftVerticalAngleMin) && pointVectorAngle <= 360){
-                return -1;//逆时针方向垂直
-            }
-        } else {
-            //min>=0&&max>=0, min<0&&max<0, 另外还有一种可能min>=0&&max<0, 在容差小于360时不可能出现, 故忽略
-            if (pointVectorAngle >= MathUtils.standardizeAngle(leftVerticalAngleMin) && pointVectorAngle <= MathUtils.standardizeAngle(leftVerticalAngleMax)){
-                return -1;//逆时针方向垂直
-            }
-        }
-
-        //顺时针方向的垂直角范围
-        float rightVerticalAngleMin = lineVectorAngle + 90 - VERTICAL_ANGLE_TOLERANCE;
-        float rightVerticalAngleMax = lineVectorAngle + 90 + VERTICAL_ANGLE_TOLERANCE;
-        if (rightVerticalAngleMin < 360 && rightVerticalAngleMax >= 360){
-            if (pointVectorAngle >= 0 && pointVectorAngle <= rightVerticalAngleMax){
-                return 1;//顺时针方向垂直
-            }
-            if (pointVectorAngle >= MathUtils.standardizeAngle(rightVerticalAngleMin) && pointVectorAngle <= 360){
-                return 1;//顺时针方向垂直
-            }
-        } else {
-            //min>=0&&max>=0, min<0&&max<0, 另外还有一种可能min>=0&&max<0, 在容差小于360时不可能出现, 故忽略
-            if (pointVectorAngle >= MathUtils.standardizeAngle(rightVerticalAngleMin) && pointVectorAngle <= MathUtils.standardizeAngle(rightVerticalAngleMax)){
-                return 1;//顺时针方向垂直
-            }
-        }
-
-        return 0;
-    }
+//    private int calculateMovingVertical(float lineVectorAngle, float pointVectorAngle){
+//        //逆时针方向的垂直角范围
+//        float leftVerticalAngleMin = lineVectorAngle - 90 - VERTICAL_ANGLE_TOLERANCE;
+//        float leftVerticalAngleMax = lineVectorAngle - 90 + VERTICAL_ANGLE_TOLERANCE;
+//        if (leftVerticalAngleMin < 0 && leftVerticalAngleMax >= 0){
+//            if (pointVectorAngle >= 0 && pointVectorAngle <= leftVerticalAngleMax){
+//                return -1;//逆时针方向垂直
+//            }
+//            if (pointVectorAngle >= MathUtils.standardizeAngle(leftVerticalAngleMin) && pointVectorAngle <= 360){
+//                return -1;//逆时针方向垂直
+//            }
+//        } else {
+//            //min>=0&&max>=0, min<0&&max<0, 另外还有一种可能min>=0&&max<0, 在容差小于360时不可能出现, 故忽略
+//            if (pointVectorAngle >= MathUtils.standardizeAngle(leftVerticalAngleMin) && pointVectorAngle <= MathUtils.standardizeAngle(leftVerticalAngleMax)){
+//                return -1;//逆时针方向垂直
+//            }
+//        }
+//
+//        //顺时针方向的垂直角范围
+//        float rightVerticalAngleMin = lineVectorAngle + 90 - VERTICAL_ANGLE_TOLERANCE;
+//        float rightVerticalAngleMax = lineVectorAngle + 90 + VERTICAL_ANGLE_TOLERANCE;
+//        if (rightVerticalAngleMin < 360 && rightVerticalAngleMax >= 360){
+//            if (pointVectorAngle >= 0 && pointVectorAngle <= rightVerticalAngleMax){
+//                return 1;//顺时针方向垂直
+//            }
+//            if (pointVectorAngle >= MathUtils.standardizeAngle(rightVerticalAngleMin) && pointVectorAngle <= 360){
+//                return 1;//顺时针方向垂直
+//            }
+//        } else {
+//            //min>=0&&max>=0, min<0&&max<0, 另外还有一种可能min>=0&&max<0, 在容差小于360时不可能出现, 故忽略
+//            if (pointVectorAngle >= MathUtils.standardizeAngle(rightVerticalAngleMin) && pointVectorAngle <= MathUtils.standardizeAngle(rightVerticalAngleMax)){
+//                return 1;//顺时针方向垂直
+//            }
+//        }
+//
+//        return 0;
+//    }
 
     private void stateToRelease() {
         motionState = MotionState.RELEASE;
@@ -346,16 +351,12 @@ public class ViewGestureControllerImpl implements ViewGestureController {
         motionState = MotionState.HOLD;
     }
 
-    private void stateToMove() {
-        motionState = MotionState.MOVE;
+    private void stateToSingleTouch() {
+        motionState = MotionState.SINGLE_TOUCH;
     }
 
-    private void stateToMoveAndZoom() {
-        motionState = MotionState.MOVE_AND_ZOOM;
-    }
-
-    private void stateToRotate() {
-        motionState = MotionState.ROTATE;
+    private void stateToMultiTouch() {
+        motionState = MotionState.MULTI_TOUCH;
     }
 
     /***************************************************************
@@ -391,9 +392,8 @@ public class ViewGestureControllerImpl implements ViewGestureController {
     private enum MotionState {
         RELEASE,//释放
         HOLD,//持有
-        MOVE,//移动
-        MOVE_AND_ZOOM,//移动和缩放
-        ROTATE//旋转
+        SINGLE_TOUCH,//单点触摸
+        MULTI_TOUCH//多点触摸
     }
 
 }
