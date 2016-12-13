a3ab648d2261cc9aac0b774fb0b8f39ce51a7cbf
shepherdviolet <ryan.n@163.com>
2016-12-13 11:26:02 +0800

    1.增加单向手势拆分检测器 2.增加手势拦截容器

---
+++ turquoise/src/main/java/sviolet/turquoise/ui/util/UnidirectionalMoveDetector.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2015-2016 S.Violet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/turquoise
+ * Email: shepherdviolet@163.com
+ */
+
+package sviolet.turquoise.ui.util;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+import static android.view.MotionEvent.ACTION_DOWN;
+import static android.view.MotionEvent.ACTION_MOVE;
+
+/**
+ * <p>MotionEvent用, 单向有效移动侦测器, 当一个方向发生有效位移后, 会保持这个状态直到触摸流程结束, 用于
+ * 拆分XY两个方向的触摸事件.</p>
+ *
+ * Created by S.Violet on 2016/11/24.
+ */
+
+public class UnidirectionalMoveDetector {
+
+    public static final int STATE_X_MOVEING = -2;//X方向移动状态
+    public static final int STATE_TO_X_MOVE = -1;//刚进入X方向移动状态
+    public static final int STATE_STATIC = 0;//静止状态
+    public static final int STATE_TO_Y_MOVE = 1;//刚进入Y方向移动状态
+    public static final int STATE_Y_MOVEING = 2;//Y方向移动状态
+
+    private float mTouchSlop;
+    private float downX, downY;
+    private int state = STATE_STATIC;
+
+    public UnidirectionalMoveDetector(Context context) {
+        this.mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    /**
+     * 持续收集MotionEvent, 并在ACTION_UP事件时判断是否发生位移, 若没有发生位移, 判断为点击事件时, 返回true
+     */
+    public int detect(MotionEvent event){
+        switch (event.getActionMasked()){
+            case ACTION_DOWN:
+                //按下事件记录按下的坐标
+                state = STATE_STATIC;
+                downX = event.getX();
+                downY = event.getY();
+                break;
+            case ACTION_MOVE:
+                switch (state){
+                    case STATE_STATIC:
+                        //发生有效位移时, 取消点击事件
+                        if (Math.abs(event.getX() - downX) > mTouchSlop){
+                            state = STATE_TO_X_MOVE;
+                        }
+                        if (Math.abs(event.getY() - downY) > mTouchSlop) {
+                            state = STATE_TO_Y_MOVE;
+                        }
+                        break;
+                    case STATE_TO_X_MOVE:
+                        state = STATE_X_MOVEING;
+                        break;
+                    case STATE_TO_Y_MOVE:
+                        state = STATE_Y_MOVEING;
+                        break;
+                }
+                break;
+        }
+        return state;
+    }
+
+}
---
+++ turquoise/src/main/java/sviolet/turquoise/ui/viewgroup/other/MotionEventTransferContainer.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2015-2016 S.Violet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/turquoise
+ * Email: shepherdviolet@163.com
+ */
+
+package sviolet.turquoise.ui.viewgroup.other;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import java.lang.ref.WeakReference;
+
+import sviolet.turquoise.ui.util.UnidirectionalMoveDetector;
+import sviolet.turquoise.util.droid.MotionEventUtils;
+
+/**
+ * Created by S.Violet on 2016/12/13.
+ */
+
+public class MotionEventTransferContainer extends FrameLayout {
+
+    private UnidirectionalMoveDetector unidirectionalMoveDetector;
+
+    private WeakReference<View> xReceiver;
+    private WeakReference<View> yReceiver;
+
+    public MotionEventTransferContainer(Context context) {
+        super(context);
+        init();
+    }
+
+    public MotionEventTransferContainer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MotionEventTransferContainer(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+        unidirectionalMoveDetector = new UnidirectionalMoveDetector(getContext());
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        int state = unidirectionalMoveDetector.detect(ev);
+        View receiver;
+
+        switch (state){
+            case UnidirectionalMoveDetector.STATE_TO_X_MOVE:
+                receiver = getXReceiver();
+                if (receiver == null){
+                    break;
+                }
+                intercept(ev, receiver);
+                return true;
+            case UnidirectionalMoveDetector.STATE_X_MOVEING:
+                receiver = getXReceiver();
+                if (receiver == null){
+                    break;
+                }
+                pass(ev, receiver);
+                return true;
+            case UnidirectionalMoveDetector.STATE_TO_Y_MOVE:
+                receiver = getYReceiver();
+                if (receiver == null){
+                    break;
+                }
+                intercept(ev, receiver);
+                return true;
+            case UnidirectionalMoveDetector.STATE_Y_MOVEING:
+                receiver = getYReceiver();
+                if (receiver == null){
+                    break;
+                }
+                pass(ev, receiver);
+                return true;
+        }
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private void intercept(MotionEvent ev, View receiver) {
+        //取消子控件的事件
+        MotionEventUtils.emulateCancelEvent(ev, emulateMotionEventExecutor);
+        //模拟接收器down事件
+        MotionEventUtils.emulateDownEvent(ev, true, receiver);
+        //分发时间给接收器
+        pass(ev, receiver);
+    }
+
+    private void pass(MotionEvent ev, View receiver) {
+        //把事件的坐标修正为接收器坐标系
+        MotionEventUtils.offsetLocation(ev, receiver);
+        //手动把事件交给接收器处理
+        receiver.dispatchTouchEvent(ev);
+        //恢复事件的坐标为本控件坐标系
+        MotionEventUtils.offsetLocation(ev, this);
+    }
+
+    public void setXReceiver(View view){
+        this.xReceiver = new WeakReference<>(view);
+    }
+
+    public void setYReceiver(View view){
+        this.yReceiver = new WeakReference<>(view);
+    }
+
+    private View getXReceiver(){
+        if (xReceiver != null){
+            return xReceiver.get();
+        }
+        return null;
+    }
+
+    private View getYReceiver(){
+        if (yReceiver != null){
+            return yReceiver.get();
+        }
+        return null;
+    }
+
+    private MotionEventUtils.EmulateMotionEventExecutor emulateMotionEventExecutor = new MotionEventUtils.EmulateMotionEventExecutor() {
+        @Override
+        public void dispatchTouchEvent(MotionEvent emulateMotionEvent) {
+            //模拟出来的事件是屏幕坐标系的, 需要根据当前ViewGroup修正坐标
+            MotionEventUtils.offsetLocation(emulateMotionEvent, MotionEventTransferContainer.this);
+            //分发事件
+            MotionEventTransferContainer.super.dispatchTouchEvent(emulateMotionEvent);
+        }
+    };
+
+}
