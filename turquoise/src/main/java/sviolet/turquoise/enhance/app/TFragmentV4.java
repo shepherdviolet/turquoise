549cb73a56df2e05d6b5f96d5c65fdfdd3934069
shepherdviolet <ryan.n@163.com>
2017-01-04 23:54:44 +0800

    lazy load

--- turquoise/src/main/java/sviolet/turquoise/enhance/app/TFragmentV4.java
+++ turquoise/src/main/java/sviolet/turquoise/enhance/app/TFragmentV4.java
@@ -42,6 +42,7 @@ public abstract class TFragmentV4 extends Fragment {
     private View fragmentViewCache;
 
     private boolean lazyLoaded = false;
+    private boolean viewInitialized = false;
     private boolean visibility = true;
 
     @Nullable
@@ -55,6 +56,7 @@ public abstract class TFragmentV4 extends Fragment {
         if(fragmentView == null){
             fragmentView = InjectUtils.inject(this, inflater, container);
             onInitView(fragmentView, savedInstanceState);
+            viewInitialized = true;
             if (fragmentViewCacheEnabled()){
                 this.fragmentViewCache = fragmentView;
             }
@@ -78,6 +80,7 @@ public abstract class TFragmentV4 extends Fragment {
         //reset lazy flag, if cache not enabled
         if (!fragmentViewCacheEnabled()){
             lazyLoaded = false;
+            viewInitialized = false;
             visibility = true;
         }
     }
@@ -127,7 +130,7 @@ public abstract class TFragmentV4 extends Fragment {
         if (isVisibleToUser){
             visibility = true;
             //lazy load if call setUserVisibleHint with true params
-            if (!lazyLoaded) {
+            if (!lazyLoaded && viewInitialized) {
                 lazyLoad();
             }
         } else {
